df5e1670ec103eb3e0c3130e6b9c3b223b565541
Huma Zia <rankwithhani@gmail.com>
2025-01-07 10:44:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// var fullName = "minahil irfan";
+// var splitingWord = fullName.split( " " );
+
+// // console.log(splitingWord);
+
+// var arr = [];
+
+// for(var i = 0; i < splitingWord.length; i++) {
+//     // console.log(splitingWord[i]);
+// }
